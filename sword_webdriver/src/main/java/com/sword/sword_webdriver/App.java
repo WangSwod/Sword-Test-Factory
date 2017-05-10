458a4def870b40dff5878e59e4dcc9af9689b043
WangSword <wangtu374@gmail.com>
2017-05-10 21:50:15 +0800

    create project

---
+++ sword_webdriver/src/main/java/com/sword/sword_webdriver/App.java
@@ -0,0 +1,13 @@
+package com.sword.sword_webdriver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
